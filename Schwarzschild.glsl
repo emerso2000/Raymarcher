2e358299c49254dccb9e37318ba06e4056b3f402
emerso2000 <83967836+emerso2000@users.noreply.github.com>
2023-05-16 17:49:32 -0400

    Update Schwarzschild.glsl
    camera finally works

--- Schwarzschild.glsl
+++ Schwarzschild.glsl
@@ -4,14 +4,23 @@ layout(local_size_x = 8, local_size_y = 4, local_size_z = 1) in;
 
 layout(rgba32f, binding = 0) uniform image2D screen;
 
-layout (std140, binding = 1) uniform CameraBlock {
+layout(std140, binding = 1) uniform CameraBlock {
     vec3 cam_o;
+    float padding1;
     vec3 forward;
+    float padding2;
     vec3 right;
+    float padding3;
     vec3 up;
+    float padding4;
     float fov;
 } camera;
 
+
+layout (std140, binding = 2) uniform MatricesBlock {
+    mat4 move;
+    mat4 view;
+} matrices;
 
 const int MAX_STEPS = 100;
 const float EPSILON = 0.001;
@@ -39,20 +48,24 @@ void main()
     ivec2 pixel_coords = ivec2(gl_GlobalInvocationID.xy);
 
     ivec2 dims = imageSize(screen);
+    float aspect_ratio = float(dims.x) / float(dims.y);
 
-    float aspect_ratio = float(dims.x) / float(dims.y); // get aspect ratio of the window
+    //normalized screen coordinates
+    float x = -(float(pixel_coords.x * 2 - dims.x) / dims.x);
+    float y = -(float(pixel_coords.y * 2 - dims.y) / dims.y);
 
-    float x = (float(pixel_coords.x * 2 - dims.x) / dims.x); // transforms to [-1.0, 1.0]
-    float y = (float(pixel_coords.y * 2 - dims.y) / dims.x); // transforms to [-1.0, 1.0]
-    
+    vec3 ray_o = vec3(x * aspect_ratio, y, 0.0);
+    // vec3 ray_d = normalize((matrices.view * vec4(ray_o, 1)).xyz);
+    vec3 ray_d = normalize(vec3(ray_o.x, ray_o.y, -1.0 / tan(camera.fov / 2.0)));
 
-    vec3 ray_o = camera.cam_o;
-    vec3 ray_d = (camera.forward + x * camera.right + y * camera.up);
-    vec3 cam_o = camera.cam_o;
+    // Apply matrices.view transformation to ray_d
+    ray_d = (matrices.view * vec4(ray_d, 0)).xyz;
 
-    float t = marchRay(cam_o, ray_d);
+    // ray_d *= aspect_ratio;
+
+    float t = marchRay(camera.cam_o, ray_d);
     if (t >= 0.0) {
-        vec3 p = cam_o + t * ray_d;
+        vec3 p = camera.cam_o + t * ray_d;
         vec3 n = normalize(vec3(
             sphereSDF(vec3(p.x + EPSILON, p.y, p.z), 1.0) - sphereSDF(vec3(p.x - EPSILON, p.y, p.z), 1.0),
             sphereSDF(vec3(p.x, p.y + EPSILON, p.z), 1.0) - sphereSDF(vec3(p.x, p.y - EPSILON, p.z), 1.0),
