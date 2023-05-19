84cc99fd4e38fd56067dc1b3e72d6cf62843c9a1
emerso2000 <83967836+emerso2000@users.noreply.github.com>
2023-05-18 20:31:34 -0400

    Update Schwarzschild.glsl

--- Schwarzschild.glsl
+++ Schwarzschild.glsl
@@ -40,161 +40,8 @@ float wallSDF(vec3 p, vec3 normal, float distance) {
 float ceilingSDF(vec3 p, float height) {
     return height - p.y;
 }
+
 //V1
-// float marchRay(vec3 origin, vec3 direction) {
-//     float t = 0.0;
-//     for (int i = 0; i < MAX_STEPS; i++) {
-//         vec3 p = origin + t * direction;
-//         float d = sphereSDF(p, 1.0);
-//         if (d < EPSILON) {
-//             return t;
-//         }
-//         t += d;
-//     }
-//     return -1.0;
-// }
-
-//V2
-// float marchRay(vec3 origin, vec3 direction) {
-//     float t = 0.0;
-//     for (int i = 0; i < MAX_STEPS; i++) {
-//         vec3 p = origin + t * direction;
-//         float d1 = sphereSDF(p, 1.0);
-//         float d2 = floorSDF(p, camera.floor_height);
-//         if (d1 < EPSILON || d2 < EPSILON) {
-//             return t;
-//         }
-//         t += min(d1, d2);
-//     }
-//     return -1.0;
-// }
-
-//V3
-// float marchRay(vec3 origin, vec3 direction) {
-//     float t = 0.0;
-//     for (int i = 0; i < MAX_STEPS; i++) {
-//         vec3 p = origin + t * direction;
-//         float d1 = sphereSDF(p, 1.0);
-//         float d2 = floorSDF(p, camera.floor_height);
-//         float d3 = wallSDF(p, vec3(0.0, 0.0, 1.0), 5.0); // 5 is distance to wall
-//         if (d1 < EPSILON || d2 < EPSILON || d3 < EPSILON) {
-//             return t;
-//         }
-//         t += min(min(d1, d2), d3);
-//     }
-//     return -1.0;
-// }
-
-//V4
-// float marchRay(vec3 origin, vec3 direction) {
-//     float t = 0.0;
-//     for (int i = 0; i < MAX_STEPS; i++) {
-//         vec3 p = origin + t * direction;
-//         float d1 = sphereSDF(p, 1.0);
-//         float d2 = floorSDF(p, camera.floor_height);
-//         float d3 = wallSDF(p, vec3(0.0, 0.0, 1.0), 3.0); // Wall 1
-//         float d4 = wallSDF(p, vec3(0.0, 1.0, 0.0), 3.0); // Wall 2
-//         float d5 = wallSDF(p, vec3(1.0, 0.0, 0.0), 3.0); // Wall 3
-//         float d6 = wallSDF(p, vec3(0.0, 0.0, -1.0), 3.0); // Wall 4
-
-//         if (d1 < EPSILON || d2 < EPSILON || d3 < EPSILON || d4 < EPSILON || d5 < EPSILON || d6 < EPSILON) {
-//             return t;
-//         }
-//         t += min(min(min(min(min(d1, d2), d3), d4), d5), d6); 
-    
-//     }
-//     return -1.0;
-// }
-
-//V5
-// float marchRay(vec3 origin, vec3 direction) {
-//     float t = 0.0;
-//     float prevDist = sphereSDF(origin, 1.0); // Distance at the starting point
-
-//     for (int i = 0; i < MAX_STEPS; i++) {
-//         vec3 p = origin + t * direction;
-//         float currentDist = sphereSDF(p, 1.0); // Distance at the current point
-
-//         if (prevDist > 0.0 && currentDist <= 0.0) {
-//             // Transition from outside to inside, indicating a hit
-//             return t;
-//         }
-
-//         t += currentDist;
-//         prevDist = currentDist;
-//     }
-
-//     return -1.0; // No hit found
-// }
-
-//V6
-// float marchRay(vec3 origin, vec3 direction) {
-//     float t = 0.0;
-//     float prevDistSphere = sphereSDF(origin, 1.0); // Distance to the sphere at the starting point
-//     float prevDistFloor = floorSDF(origin, camera.floor_height); // Distance to the floor at the starting point
-
-//     for (int i = 0; i < MAX_STEPS; i++) {
-//         vec3 p = origin + t * direction;
-//         float currentDistSphere = sphereSDF(p, 1.0); // Distance to the sphere at the current point
-//         float currentDistFloor = floorSDF(p, camera.floor_height); // Distance to the floor at the current point
-
-//         if (prevDistSphere > 0.0 && currentDistSphere <= 0.0) {
-//             // Transition from outside to inside the sphere, indicating a hit
-//             return t;
-//         }
-
-//         if (prevDistFloor > 0.0 && currentDistFloor <= 0.0) {
-//             // Transition from outside to inside the floor, indicating a hit
-//             return t;
-//         }
-
-//         t += min(currentDistSphere, currentDistFloor); // Take the minimum distance for marching
-
-//         prevDistSphere = currentDistSphere;
-//         prevDistFloor = currentDistFloor;
-//     }
-
-//     return -1.0; // No hit found
-// }
-
-//V7
-// float marchRay(vec3 origin, vec3 direction) {
-//     float t = 0.0;
-//     float prevDistSphere = sphereSDF(origin, 1.0); // Distance to the sphere at the starting point
-//     float prevDistFloor = floorSDF(origin, camera.floor_height); // Distance to the floor at the starting point
-
-//     float minStepSize = 0.001; // Minimum step size
-
-//     for (int i = 0; i < MAX_STEPS; i++) {
-//         vec3 p = origin + t * direction;
-//         float currentDistSphere = sphereSDF(p, 1.0); // Distance to the sphere at the current point
-//         float currentDistFloor = floorSDF(p, camera.floor_height); // Distance to the floor at the current point
-
-//         if (prevDistSphere > 0.0 && currentDistSphere <= 0.0) {
-//             // Transition from outside to inside the sphere, indicating a hit
-//             return t;
-//         }
-
-//         if (prevDistFloor > 0.0 && currentDistFloor <= 0.0) {
-//             // Transition from outside to inside the floor, indicating a hit
-//             return t;
-//         }
-
-//         float stepSize = min(currentDistSphere, currentDistFloor);
-//         if (stepSize < minStepSize) {
-//             stepSize = minStepSize; // Limit the step size to the minimum value
-//         }
-
-//         t += stepSize;
-
-//         prevDistSphere = currentDistSphere;
-//         prevDistFloor = currentDistFloor;
-//     }
-
-//     return -1.0; // No hit found
-// }
-
-//V8
 float marchRay(vec3 origin, vec3 direction) {
     float t = 0.0;
     float prevDistSphere = sphereSDF(origin, 1.0);
@@ -263,7 +110,6 @@ float marchRay(vec3 origin, vec3 direction) {
     return -1.0;
 }
 
-
 void main()
 {
     vec4 pixel = vec4(0.115, 0.133, 0.173, 1.0);
@@ -285,111 +131,15 @@ void main()
     float t = marchRay(camera.cam_o, ray_d);
 
     //V1
-    // if (t >= 0.0) {
-    //     vec3 p = camera.cam_o + t * ray_d;
-    //     vec3 n = normalize(vec3(
-    //         sphereSDF(vec3(p.x + EPSILON, p.y, p.z), 1.0) - sphereSDF(vec3(p.x - EPSILON, p.y, p.z), 1.0),
-    //         sphereSDF(vec3(p.x, p.y + EPSILON, p.z), 1.0) - sphereSDF(vec3(p.x, p.y - EPSILON, p.z), 1.0),
-    //         sphereSDF(vec3(p.x, p.y, p.z + EPSILON), 1.0) - sphereSDF(vec3(p.x, p.y, p.z - EPSILON), 1.0)
-    //     ));
-    //     pixel = vec4((n + 1.0) / 2.0, 1.0);
-    // }
-
-    //V2
-    // if (t >= 0.0) {
-    //     vec3 p = camera.cam_o + t * ray_d;
-    //     vec3 n = normalize(vec3(
-    //         sphereSDF(vec3(p.x + EPSILON, p.y, p.z), 1.0) - sphereSDF(vec3(p.x - EPSILON, p.y, p.z), 1.0),
-    //         sphereSDF(vec3(p.x, p.y + EPSILON, p.z), 1.0) - sphereSDF(vec3(p.x, p.y - EPSILON, p.z), 1.0),
-    //         sphereSDF(vec3(p.x, p.y, p.z + EPSILON), 1.0) - sphereSDF(vec3(p.x, p.y, p.z - EPSILON), 1.0)
-    //     ));
-        // if (floorSDF(p, camera.floor_height) < EPSILON) {
-        //     n = vec3(0.0, -1.0, 0.0); // Set the normal vector for the floor surface
-        // }
-    //     pixel = vec4((n + 1.0) / 2.0, 1.0);
-    // }
-
-    //V3
-    // if (t >= 0.0) {
-    //     vec3 p = camera.cam_o + t * ray_d;
-    //     vec3 n = normalize(vec3(
-    //         sphereSDF(vec3(p.x + EPSILON, p.y, p.z), 1.0) - sphereSDF(vec3(p.x - EPSILON, p.y, p.z), 1.0),
-    //         sphereSDF(vec3(p.x, p.y + EPSILON, p.z), 1.0) - sphereSDF(vec3(p.x, p.y - EPSILON, p.z), 1.0),
-    //         sphereSDF(vec3(p.x, p.y, p.z + EPSILON), 1.0) - sphereSDF(vec3(p.x, p.y, p.z - EPSILON), 1.0)
-    //     ));
-    //     if (floorSDF(p, camera.floor_height) < EPSILON) {
-    //         n = vec3(0.0, -1.0, 0.0); // Set the normal vector for the floor surface
-    //     } 
-    //     else if (wallSDF(p, vec3(0.0, 0.0, 1.0), 5.0) < EPSILON) { // Check if point is on the wall
-    //         n = vec3(1.0, 0.0, 0.0); // Set the normal vector for the wall surface
-    //     }
-
-    //     pixel = vec4((n + 1.0) / 2.0, 1.0);
-    // }
-
-    //V4
-    // if (t >= 0.0) {
-    //     vec3 p = camera.cam_o + t * ray_d;
-    //     vec3 n = normalize(vec3(
-    //         sphereSDF(vec3(p.x + EPSILON, p.y, p.z), 1.0) - sphereSDF(vec3(p.x - EPSILON, p.y, p.z), 1.0),
-    //         sphereSDF(vec3(p.x, p.y + EPSILON, p.z), 1.0) - sphereSDF(vec3(p.x, p.y - EPSILON, p.z), 1.0),
-    //         sphereSDF(vec3(p.x, p.y, p.z + EPSILON), 1.0) - sphereSDF(vec3(p.x, p.y, p.z - EPSILON), 1.0)
-    //     ));
-    //     if (floorSDF(p, camera.floor_height) < EPSILON) {
-    //         n = vec3(0.0, -1.0, 0.0); // Set the normal vector for the floor surface
-    //     } 
-    //     else if (wallSDF(p, vec3(0.0, 0.0, 1.0), 3.0) < EPSILON) { // Check if point is on the first wall
-    //         n = vec3(0.0, 0.0, -1.0); // Set the normal vector for the wall surface
-    //     }
-    //     else if (wallSDF(p, vec3(0.0, 1.0, 0.0), 3.0) < EPSILON) { // Check if point is on the second wall
-    //         n = vec3(0.0, -1.0, 0.0); // Set the normal vector for the wall surface
-    //     }
-    //     else if (wallSDF(p, vec3(1.0, 0.0, 0.0), 3.0) < EPSILON) { // Check if point is on the third wall
-    //         n = vec3(-1.0, 0.0, 0.0); // Set the normal vector for the wall surface
-    //     }
-    //     else if (wallSDF(p, vec3(0.0, 0.0, -1.0), 3.0) < EPSILON) { // Check if point is on the wall behind the camera
-    //         n = vec3(0.0, 0.0, 1.0); // Set the normal vector for the wall surface
-    //     }
-
-    //     pixel = vec4((n + 1.0) / 2.0, 1.0);
-    // }
-
-    //V5
-    // if (t >= 0.0) {
-    //     vec3 p = camera.cam_o + t * ray_d;
-    //     vec3 color = vec3(1.0, 0.0, 0.0); // Solid red color
-    //     pixel = vec4(color, 1.0);
-    // }
-
-    //V6
-    // if (t >= 0.0) {
-    //     vec3 p = camera.cam_o + t * ray_d;
-    //     vec3 sphereColor = vec3(1.0, 0.0, 0.0); // Red color for the sphere
-    //     vec3 floorColor = vec3(0.0, 0.0, 1.0); // Blue color for the floor
-
-    //     // Check if the hit point is closer to the sphere or the floor
-    //     float sphereDist = sphereSDF(p, 1.0);
-    //     float floorDist = floorSDF(p, camera.floor_height);
-
-    //     if (sphereDist > floorDist) {
-    //         // The hit point is closer to the floor
-    //         pixel = vec4(floorColor, 1.0);
-    //     } else {
-    //         // The hit point is closer to the sphere
-    //         pixel = vec4(sphereColor, 1.0);
-    //     }
-    // }
-
-    //V8
     if (t >= 0.0) {
         vec3 p = camera.cam_o + t * ray_d;
         vec3 sphereColor = vec3(1.0, 0.0, 0.0); // Red color for the sphere
-        vec3 floorColor = vec3(1.0, 1.0, 1.0); // Blue color for the floor
-        vec3 ceilingColor = vec3(0.0, 0.0, 1.0); // White color for the ceiling
-        vec3 wallColor1 = vec3(0.0, 1.0, 0.0); // Green color for the first wall
-        vec3 wallColor2 = vec3(1.0, 1.0, 0.0); // Yellow color for the second wall
-        vec3 wallColor3 = vec3(1.0, 0.0, 1.0); // Magenta color for the third wall
-        vec3 wallColor4 = vec3(0.0, 1.0, 1.0); // Cyan color for the fourth wall
+        vec3 floorColor = vec3(0.0, 0.0, 1.0); // green color for the floor
+        vec3 ceilingColor = vec3(0.0, 1.0, 0.0); // blue color for the ceiling
+        vec3 wallColor1 = vec3(1.0, 1.0, 0.0); // yellow color for the first wall
+        vec3 wallColor2 = vec3(0.0, 1.0, 1.0); // cyan color for the second wall
+        vec3 wallColor3 = vec3(0.5, 0.0, 0.5); // magenta color for the third wall
+        vec3 wallColor4 = vec3(1.0, 0.5, 1.0); // orange color for the fourth wall
 
         float sphereDist = sphereSDF(p, 1.0);
         float floorDist = floorSDF(p, camera.floor_height);
