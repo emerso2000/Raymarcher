5519f93e660bb1df3d93a4972aaa977e45cf8d94
emerso2000 <83967836+emerso2000@users.noreply.github.com>
2023-05-30 21:34:13 -0400

    not working spherical coords

--- Schwarzschild.glsl
+++ Schwarzschild.glsl
@@ -21,7 +21,7 @@ layout (std140, binding = 2) uniform MatricesBlock {
     mat4 view;
 } matrices;
 
-const int MAX_STEPS = 7000;
+const int MAX_STEPS = 5000;
 
 float sphereSDF(vec3 p, float r) {
     return length(p) - r;
@@ -39,16 +39,115 @@ float ceilingSDF(vec3 p, float height) {
     return height - p.y;
 }
 
+vec3 cartesianToSpherical(vec3 cartesian) {
+    float radius = length(cartesian);
+    float theta = acos(cartesian.z / radius);
+    float phi = atan(cartesian.y, cartesian.x);
+
+    return vec3(radius, theta, phi);
+}
+
+vec3 sphericalToCartesian(vec3 spherical) {
+    float x = spherical.x * sin(spherical.y) * cos(spherical.z);
+    float y = spherical.x * sin(spherical.y) * sin(spherical.z);
+    float z = spherical.x * cos(spherical.y);
+
+    return vec3(x, y, z);
+}
+
+vec3 cartesianToAzELR(vec3 cartesianVec, vec3 newRayOrigin) {
+    // float r = sqrt((newRayOrigin.x * newRayOrigin.x) + (newRayOrigin.y * newRayOrigin.y) + (newRayOrigin.z * newRayOrigin.z));
+    // float az = acos(newRayOrigin.z / r);
+    // float el = newRayOrigin.z;
+
+    float r = newRayOrigin.x;
+    float az = newRayOrigin.y;
+    float el = newRayOrigin.z;
+
+    mat3 transformationMatrix = mat3(
+        -sin(az),  cos(az),  0.0,
+        -sin(el) * cos(az), -sin(el) * sin(az),  cos(el),
+        cos(el) * cos(az),  cos(el) * sin(az),  sin(el)
+    );
+
+    vec3 newVec = transformationMatrix * cartesianVec;
+
+    return newVec;
+}
+
+mat3 calculateChristoffelSymbolsAlphaR(vec3 position) {
+    float r = position.x;
+    float theta = position.y;
+
+    mat3 christoffelSymbols_alpha_r;
+
+    float rs = 0.0; // Schwarzschild radius
+
+    christoffelSymbols_alpha_r[0][0] = -rs / (2.0 * r) * (r - rs);
+    christoffelSymbols_alpha_r[0][1] = 0.0;
+    christoffelSymbols_alpha_r[0][2] = 0.0;
+
+    christoffelSymbols_alpha_r[1][0] = 0.0;
+    christoffelSymbols_alpha_r[1][1] = rs - r;
+    christoffelSymbols_alpha_r[1][2] = 0.0;
+
+    christoffelSymbols_alpha_r[2][0] = 0.0;
+    christoffelSymbols_alpha_r[2][1] = 0.0;
+    christoffelSymbols_alpha_r[2][2] = (rs - r) * sin(theta) * sin(theta);
+
+    return christoffelSymbols_alpha_r;
+}
+
+mat3 calculateChristoffelSymbolsAlphaTheta(vec3 position) {
+    float r = position.x;
+    float theta = position.y;
+
+    mat3 christoffelSymbols_alpha_theta;
+
+    float rs = 0.0; // Schwarzschild radius
+
+    christoffelSymbols_alpha_theta[0][0] = 0.0;
+    christoffelSymbols_alpha_theta[0][1] = 1.0 / r;
+    christoffelSymbols_alpha_theta[0][2] = 0.0;
+
+    christoffelSymbols_alpha_theta[1][0] = 1.0 / r;
+    christoffelSymbols_alpha_theta[1][1] = 0.0;
+    christoffelSymbols_alpha_theta[1][2] = 0.0;
+
+    christoffelSymbols_alpha_theta[2][0] = 0.0;
+    christoffelSymbols_alpha_theta[2][1] = 0.0;
+    christoffelSymbols_alpha_theta[2][2] = -sin(theta) * cos(theta);
+
+    return christoffelSymbols_alpha_theta;
+}
+
+mat3 calculateChristoffelSymbolsAlphaPhi(vec3 position) {
+    float r = position.x;
+    float theta = position.y;
+
+    mat3 christoffelSymbols_alpha_phi;
+
+    float rs = 0.0; // Schwarzschild radius
+
+    christoffelSymbols_alpha_phi[0][0] = 0.0;
+    christoffelSymbols_alpha_phi[0][1] = 1.0 / r;
+    christoffelSymbols_alpha_phi[0][2] = 0.0;
+
+    christoffelSymbols_alpha_phi[1][0] = 1.0 / r;
+    christoffelSymbols_alpha_phi[1][1] = 0.0;
+    christoffelSymbols_alpha_phi[1][2] = 1.0 / tan(theta);
+
+    christoffelSymbols_alpha_phi[2][0] = 0.0;
+    christoffelSymbols_alpha_phi[2][1] = 1.0 / tan(theta);
+    christoffelSymbols_alpha_phi[2][2] = 0.0;
+
+    return christoffelSymbols_alpha_phi;
+}
+
 float marchRay(vec3 origin, vec3 direction) {
     float t = 0.0;
 
-    float prevDistSphere = sphereSDF(origin, 1.0);
-    float prevDistFloor = floorSDF(origin, camera.floor_height);
-    float prevDistCeiling = ceilingSDF(origin, camera.ceiling_height);
-    float prevDistWall1 = wallSDF(origin, vec3(0.0, 0.0, 1.0), 3.0); // Distance to the first wall
-    float prevDistWall2 = wallSDF(origin, vec3(0.0, 0.0, -1.0), 3.0); // Distance to the second wall
-    float prevDistWall3 = wallSDF(origin, vec3(1.0, 0.0, 0.0), 3.0); // Distance to the third wall
-    float prevDistWall4 = wallSDF(origin, vec3(-1.0, 0.0, 0.0), 3.0); // Distance to the fourth wall
+    float prevDistSphere = sphereSDF(sphericalToCartesian(origin), 1.0);
 
     float minStepSize = 0.001;
 
@@ -56,57 +155,44 @@ float marchRay(vec3 origin, vec3 direction) {
 
     vec3 p = origin;
 
+    vec3 accel = vec3(0.0);
+
     for (int i = 0; i < MAX_STEPS; i++) {
         p += stepSize * direction;
+        vec3 p_cart = sphericalToCartesian(p);
 
-        float currentDistSphere = sphereSDF(p, 1.0);
-        float currentDistFloor = floorSDF(p, camera.floor_height);
-        float currentDistCeiling = ceilingSDF(p, camera.ceiling_height);
-        float currentDistWall1 = wallSDF(p, vec3(0.0, 0.0, 1.0), 3.0); // Distance to the first wall
-        float currentDistWall2 = wallSDF(p, vec3(0.0, 0.0, -1.0), 3.0); // Distance to the second wall
-        float currentDistWall3 = wallSDF(p, vec3(1.0, 0.0, 0.0), 3.0); // Distance to the third wall
-        float currentDistWall4 = wallSDF(p, vec3(-1.0, 0.0, 0.0), 3.0); // Distance to the fourth wall
+        float currentDistSphere = sphereSDF(p_cart, 1.0);
 
         if (prevDistSphere > 0.0 && currentDistSphere <= 0.0) {
             return t;
         }
-        if (prevDistFloor > 0.0 && currentDistFloor <= 0.0) {
-            return t;
-        }
-        if (prevDistCeiling > 0.0 && currentDistCeiling <= 0.0) {
-            return t;
-        }
-        if (prevDistWall1 > 0.0 && currentDistWall1 <= 0.0) {
-            return t;
-        }
-        if (prevDistWall2 > 0.0 && currentDistWall2 <= 0.0) {
-            return t;
-        }
-        if (prevDistWall3 > 0.0 && currentDistWall3 <= 0.0) {
-            return t;
-        }
-        if (prevDistWall4 > 0.0 && currentDistWall4 <= 0.0) {
-            return t;
-        }
-        stepSize = min(min(min(min(min(min(currentDistSphere, currentDistFloor), currentDistCeiling), currentDistWall1), currentDistWall2), currentDistWall3), currentDistWall4);
-        
+
+        stepSize = currentDistSphere;
         if (stepSize < minStepSize) {
             stepSize = minStepSize;
         }
+
+        mat3 christoffelSymbols_alpha_r = calculateChristoffelSymbolsAlphaR(p);
+        mat3 christoffelSymbols_alpha_theta = calculateChristoffelSymbolsAlphaTheta(p);
+        mat3 christoffelSymbols_alpha_phi = calculateChristoffelSymbolsAlphaPhi(p);
+
+        // Calculate the accelerations using the geodesic equation
+        accel.x = -dot(direction, christoffelSymbols_alpha_r * direction);
+        accel.y = -dot(direction, christoffelSymbols_alpha_theta * direction);
+        accel.z = -dot(direction, christoffelSymbols_alpha_phi * direction);
+
+        direction += accel * stepSize;
+
         t += stepSize;
+
         prevDistSphere = currentDistSphere;
-        prevDistFloor = currentDistFloor;
-        prevDistCeiling = currentDistCeiling;
-        prevDistWall1 = currentDistWall1;
-        prevDistWall2 = currentDistWall2;
-        prevDistWall3 = currentDistWall3;
-        prevDistWall4 = currentDistWall4;
+
     }
     return -1.0;
 }
 
-void main()
-{
+
+void main() {
     vec4 pixel = vec4(0.115, 0.133, 0.173, 1.0);
     ivec2 pixel_coords = ivec2(gl_GlobalInvocationID.xy);
 
@@ -115,54 +201,34 @@ void main()
     vec2 uv = (vec2(pixel_coords) - 0.5 * dims.xy) / dims.y;
 
     vec3 ro = camera.cam_o;
+
     vec3 rd = (vec3(uv.x, uv.y, 1.0));
 
     rd = (matrices.view * vec4(rd, 0)).xyz;
-    rd = normalize(rd);
 
-    float d = marchRay(ro, rd);
+    rd = normalize(rd);
+    
+    vec3 sphericalRo = cartesianToSpherical(ro);
+    vec3 sphericalRd = cartesianToAzELR(rd, sphericalRo);
+
+    sphericalRd.y /= sphericalRo.x;
+
+    sphericalRd.z /= (sphericalRo.x * sin(sphericalRo.y));
+
+    float d = marchRay(sphericalRo, sphericalRd);
+
+    vec3 p = sphericalRo;
 
     if (d >= 0.0) {
-        vec3 p = ro + d * rd;
+        p += sphericalRd * d;
+
+        vec3 p_cart = sphericalToCartesian(p);
 
         vec3 sphereColor = vec3(1.0, 0.0, 0.0); // Red color for the sphere
-        vec3 floorColor = vec3(0.0, 0.0, 1.0); // green color for the floor
-        vec3 ceilingColor = vec3(0.0, 1.0, 0.0); // blue color for the ceiling
-        vec3 wallColor1 = vec3(1.0, 1.0, 0.0); // yellow color for the first wall
-        vec3 wallColor2 = vec3(0.0, 1.0, 1.0); // cyan color for the second wall
-        vec3 wallColor3 = vec3(0.5, 0.0, 0.5); // magenta color for the third wall
-        vec3 wallColor4 = vec3(1.0, 0.5, 1.0); // orange color for the fourth wall
 
-        float sphereDist = sphereSDF(p, 1.0);
-        float floorDist = floorSDF(p, camera.floor_height);
-        float ceilingDist = ceilingSDF(p, camera.ceiling_height);
-        float wallDist1 = wallSDF(p, vec3(0.0, 0.0, 1.0), 3.0); // Distance to the first wall
-        float wallDist2 = wallSDF(p, vec3(0.0, 0.0, -1.0), 3.0); // Distance to the second wall
-        float wallDist3 = wallSDF(p, vec3(1.0, 0.0, 0.0), 3.0); // Distance to the third wall
-        float wallDist4 = wallSDF(p, vec3(-1.0, 0.0, 0.0), 3.0); // Distance to the fourth wall
+        float sphereDist = sphereSDF(p_cart, 1.0);
 
-        if (sphereDist < floorDist && sphereDist < ceilingDist && sphereDist < wallDist1 && sphereDist < wallDist2 && sphereDist < wallDist3 && sphereDist < wallDist4) {
-            // The hit point is closer to the sphere
-            pixel = vec4(sphereColor, 1.0);
-        } else if (floorDist < ceilingDist && floorDist < wallDist1 && floorDist < wallDist2 && floorDist < wallDist3 && floorDist < wallDist4) {
-            // The hit point is closer to the floor
-            pixel = vec4(floorColor, 1.0);
-        } else if (ceilingDist < wallDist1 && ceilingDist < wallDist2 && ceilingDist < wallDist3 && ceilingDist < wallDist4) {
-            // The hit point is closer to the ceiling
-            pixel = vec4(ceilingColor, 1.0);
-        } else if (wallDist1 < wallDist2 && wallDist1 < wallDist3 && wallDist1 < wallDist4) {
-            // The hit point is closer to the first wall
-            pixel = vec4(wallColor1, 1.0);
-        } else if (wallDist2 < wallDist3 && wallDist2 < wallDist4) {
-            // The hit point is closer to the second wall
-            pixel = vec4(wallColor2, 1.0);
-        } else if (wallDist3 < wallDist4) {
-            // The hit point is closer to the third wall
-            pixel = vec4(wallColor3, 1.0);
-        } else {
-            // The hit point is closer to the fourth wall
-            pixel = vec4(wallColor4, 1.0);
-        }
+        pixel = vec4(sphereColor, 1.0);
     }
     
     imageStore(screen, pixel_coords, pixel);
